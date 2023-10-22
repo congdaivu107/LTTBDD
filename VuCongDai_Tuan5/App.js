b0952c64be6567cea01e788e4692bce0174b08cd
congdaivu107 <94149608+congdaivu107@users.noreply.github.com>
2023-10-22 21:50:01 +0700

    Update App.js

--- VuCongDai_Tuan5/App.js
+++ VuCongDai_Tuan5/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import S1 from './scr/S1'
+import Screen2 from './scr/S2'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <stack.Navigator mode='modal'>
+      <stack.scr options={ {headerShown:false}} name='Screen1' component={S1}></stack.Screen>
+      <stack.scr options={ {headerShown:false}} name='Screen2' component={S2}></stack.Screen>
+    </stack.Navigator>
+   </NavigationContainer>
   );
 }
 
